77bff383b7520f26cc092729b5a7c4f42843808c
Eric Harris <eahmedia22@gmail.com>
2025-04-01 15:41:28 -0400

    Fixing errors with export endpoint

--- api/export.mjs
+++ api/export.mjs
@@ -2,12 +2,12 @@ import {startGGBracket} from "./server_modules/startgg.js"
 import {getTournamentInfo} from "./server_modules/challonge.js"
 
 export async function POST (request) {
-  console.log(request)
-  if (request.body.bracket.includes('start.gg')) {
-    const obf = await startGGBracket(request.body.bracket.trim())
+  const body = await request.json()
+  if (body.bracket.includes('start.gg')) {
+    const obf = await startGGBracket(body.bracket.trim())
     return Response.json(obf)
-  } else if (request.body.bracket.includes('challonge.com')) {
-    const obf = await getTournamentInfo(request.body.bracket.trim())
+  } else if (body.bracket.includes('challonge.com')) {
+    const obf = await getTournamentInfo(body.bracket.trim())
     return Response.json(obf)
   } else {
     return Response.json({ error : "UNSUPPORTED SITE" })
