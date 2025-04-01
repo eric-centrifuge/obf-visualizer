644fa53eb8e8bc677e344f7956dc7c8b222c07ee
Eric Harris <eahmedia22@gmail.com>
2025-04-01 14:23:16 -0400

    Attempting to fix function errors

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function GET(request: Request) {
+export async function GET(request: Request) {
     const response = await fetch('https://api.vercel.app/products')
     const products = await response.json()
     return Response.json(products)
