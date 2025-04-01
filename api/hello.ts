ee5abbfd985c5aaa9decd198522beedb1310471d
Eric Harris <eahmedia22@gmail.com>
2025-04-01 14:17:31 -0400

    Adding max width to text in name card

---
+++ api/hello.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function GET(request: Request) {
+    const response = await fetch('https://api.vercel.app/products')
+    const products = await response.json()
+    return Response.json(products)
+}
